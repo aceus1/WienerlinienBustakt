e6dbd4b10007062dc7c7acf0e1b3ec3ef4458006
Daniel <daniel_2401@gmx.at>
2017-03-21 20:39:57 +0100

    Initial Commit

---
+++ BustaktConsole/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BustaktConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anzahlbus = 0;//Busvoll
+            List<Schueler> wartende = new List<Schueler>();
+            //Delta T = 1 min
+            for (int i = 0; i <= 30; i++)// 0 = 7:30; 30 = 8:00
+            {
+                int anzahladd = 0;
+                switch (i)
+                {
+                    case 0:
+                        anzahladd = 20;
+                        break;
+                    case 3:
+                        anzahladd = 25;
+                        break;
+                    case 6:
+                        anzahladd = 30;
+                        break;
+                    case 9:
+                        anzahladd = 70;
+                        break;
+                    case 12:
+                        anzahladd = 90;
+                        break;
+                    case 15:
+                        anzahladd = 100;
+                        break;
+                    case 18:
+                        anzahladd = 110;
+                        break;
+                    case 21:
+                        anzahladd = 80;
+                        break;
+                    case 24:
+                        anzahladd = 60;
+                        break;
+                    case 27:
+                        anzahladd = 60;
+                        break;
+                    case 29:
+                        anzahladd = 50;
+                        break;
+                }
+                for (int j = 0; j < anzahladd; j++)
+                {
+                    wartende.Add(new Schueler());
+                }
+                int tmpcount = 0;
+                for (int j = 0; j < wartende.Count; j++)
+                {
+                    if (wartende[j].gehzeit > 0)
+                    {
+                        wartende[j].gehzeit--;
+                    }
+                    if (wartende[j].gehzeit <= 0)
+                    {
+                        tmpcount++;
+                        if (anzahlbus < 90)
+                        {
+                            anzahlbus++;
+                            wartende.RemoveAt(j);
+                        }
+                    }
+                }
+                if (i % 5 == 0)
+                {
+                    anzahlbus = 0;
+                }
+                Console.WriteLine(anzahlbus);
+                if (i % 10 == 0)
+                {
+                    Console.WriteLine($"Momentan warten beim 94A: {anzahlbus + tmpcount}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BustaktConsole/Schueler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BustaktConsole
+{
+    class Schueler
+    {
+        static int Countid = 0;
+        static bool switcher = true; //true is schnell(3min) false is langsam(7min)
+        public int id { get; private set; }
+        public int gehzeit { get; set; }
+        public Schueler()
+        {
+            id = Countid;
+            Countid++;
+            if (switcher)
+            {
+                switcher = false;
+                gehzeit = 3;
+            }
+            else
+            {
+                switcher = true;
+                gehzeit = 7;
+            }
+        }
+    }
+}
